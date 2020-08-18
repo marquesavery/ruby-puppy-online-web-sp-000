e5d702519a8e6774e944ad41af319a0bc33be033
Marques Avery <averymarques4@gmail.com>
2020-08-18 20:51:00 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,37 @@
 # Add your code here
+require 'pry'
+
+class Dog
+
+  @@all = []
+
+  attr_reader :name
+
+
+  def initialize(name)
+    @name = name
+    # binding.pry
+    save
+  end
+
+  def save
+    # binding.pry
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.print_all
+    @@all.each do |name|
+      puts name.instance_variable_get(:@name)
+    end
+  end
+
+
+end
