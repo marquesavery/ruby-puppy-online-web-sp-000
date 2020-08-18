9dfc94d8402af6375643914d032105601b36c8ba
Marques Avery <averymarques4@gmail.com>
2020-08-18 19:35:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+class Dog
+
+  @@all = []
+
+
+  def initialize(name)
+    @name = name
+    save(name)
+  end
+
+  def save(name)
+    @@all << name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+
+  end
+
+  def self.print_all
+    @@all.each do |name|
+      puts name
+    end
+  end
+
+
+end
